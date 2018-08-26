c9afc4384eca805eb2d1972229cb5b27de741c81
leon846666 <leon846666@gmail.com>
2018-08-26 16:07:35 -0400

    JMS in Queue model

---
+++ src/main/java/com/leon/QueueProducer.java
@@ -0,0 +1,49 @@
+package com.leon;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class QueueProducer {
+
+	public static void main(String[] args) throws JMSException {
+		
+		// 1.create connection factory
+		ConnectionFactory factory = new ActiveMQConnectionFactory("tcp://192.168.25.131:61616");
+		
+		// 2.create connection object
+		Connection connection = factory.createConnection();
+		
+		// 3. start connection
+		connection.start();
+		
+		// 4. get session
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		// 5. create queue object
+		Queue queue = session.createQueue("test-queue");
+		
+		// 6. create msg provider obj
+		MessageProducer producer = session.createProducer(queue);
+		
+		// 7. create text msg
+		TextMessage textMessage = session.createTextMessage("hello world!");
+		
+		// 8. provider send msg
+		producer.send(textMessage);
+		
+		// 9. close resources;
+		producer.close();
+		session.close();
+		connection.close();
+		
+		
+		
+	}
+}
