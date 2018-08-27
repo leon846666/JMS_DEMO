8bf822509d785cab4228dbcfb17f28a948c94f8c
leon846666 <leon846666@gmail.com>
2018-08-27 09:12:02 -0400

    JMS in Topic model

---
+++ src/main/java/com/leon/TopicConsumer.java
@@ -0,0 +1,65 @@
+package com.leon;
+
+import java.io.IOException;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class TopicConsumer {
+	public static void main(String[] args) throws JMSException, IOException {
+		// 1.create connection factory
+		ConnectionFactory factory = new ActiveMQConnectionFactory("tcp://192.168.25.131:61616");
+		
+		// 2.create connection object
+		Connection connection = factory.createConnection();
+		
+		// 3. start connection
+		connection.start();
+		
+		// 4. get session
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		// 5. create queue object
+		Topic topic = session.createTopic("test-topic");
+		
+		// 6. create consumer
+		MessageConsumer consumer = session.createConsumer(topic);
+		
+		// 7. set listener
+		consumer.setMessageListener(new MessageListener() {
+			
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+					System.out.println("got the msg :"+textMessage.getText());
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		// 8.pause for a while to listener
+		System.in.read();
+		
+		
+		// 9. close resources;
+		consumer.close();
+		session.close();
+		connection.close();
+		
+		
+		
+	}
+}
